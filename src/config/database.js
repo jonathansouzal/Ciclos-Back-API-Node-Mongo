0a5c7884a975fafc673a724d9053c3b336603f0b
jonathan <jonathandesenho@gmail.com>
2020-05-04 03:35:03 -0300

    url mongo

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ mongoose.Promise = global.Promise
 
 mongoose.Error.messages.general.required = "O atributa '{PATH}' é obrigatório."
 
-const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://ciclosapp:@Js030508@ds041651.mlab.com:41651/heroku_8qn4wl8r'
+const url = 'mongodb://ciclosapp:@Js030508@ds041651.mlab.com:41651/heroku_8qn4wl8r'
 // const url = process.env.MONGOLAB_URI ? process.env.MONGOLAB_URI : 'mongodb://localhost/mymoney'
 
 module.exports = mongoose.connect(url, { useNewUrlParser: true })
